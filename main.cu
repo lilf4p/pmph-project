1917e4237abb90b4be3fab05884e20ec21851407
Leonardo Stoppani <58340900+lilf4p@users.noreply.github.com>
2023-11-03 17:25:02 +0100

    minor

--- main.cu
+++ main.cu
@@ -76,12 +76,12 @@ int bandwidthCudaMemcpy( const size_t   N     // length of the input array
 // Function that benchmark and validate the single pass scan 
 // Return the gigaBytesPerSec of the sps 
 template<class OP>
-int spScanInc( const uint32_t B     // desired CUDA block size ( <= 1024, multiple of 32)
+int spScanInc( uint32_t B     // desired CUDA block size ( <= 1024, multiple of 32)
                    , uint32_t N     // length of the input array
                    , int* h_in            // host input    of size: N * sizeof(int)
                    , int* d_in            // device input  of size: N * sizeof(int)
                    , int* d_out           // device result of size: N * sizeof(int)
-                   , const uint32_t kernel_version    // scan kernel version
+                   , uint32_t kernel_version    // scan kernel version
                    , const uint32_t chunk 
                    , int validate     
 ) {
@@ -255,10 +255,10 @@ int main (int argc, char * argv[]) {
     initHwd();
 
     // Configuration from main call 
-    const uint8_t BENCHMARK = atoi(argv[1]);
-    const uint32_t N = atoi(argv[2]);
-    const uint32_t B = atoi(argv[3]);
-    const uint32_t KERNEL = atoi(argv[4]);
+    uint8_t BENCHMARK = atoi(argv[1]);
+    uint32_t N = atoi(argv[2]);
+    uint32_t B = atoi(argv[3]);
+    uint32_t KERNEL = atoi(argv[4]);
     const uint32_t CHUNK = 12;
 
     int* h_in;

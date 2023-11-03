d53145bbbce4a23225979a069faee3f365666141
Leonardo Stoppani <58340900+lilf4p@users.noreply.github.com>
2023-11-03 17:37:24 +0100

    minor

--- main.cu
+++ main.cu
@@ -76,8 +76,8 @@ int bandwidthCudaMemcpy( const size_t   N     // length of the input array
 // Function that benchmark and validate the single pass scan 
 // Return the gigaBytesPerSec of the sps 
 template<class OP, uint8_t CHUNK>
-int spScanInc( const uint32_t B     // desired CUDA block size ( <= 1024, multiple of 32)
-                   , const uint32_t N     // length of the input array
+int spScanInc( uint32_t B     // desired CUDA block size ( <= 1024, multiple of 32)
+                   , uint32_t N     // length of the input array
                    , int* h_in            // host input    of size: N * sizeof(int)
                    , int* d_in            // device input  of size: N * sizeof(int)
                    , int* d_out           // device result of size: N * sizeof(int)
@@ -255,7 +255,7 @@ int main (int argc, char * argv[]) {
     // Configuration from main call 
     uint8_t BENCHMARK = atoi(argv[1]);
     uint32_t N = atoi(argv[2]);
-    const uint32_t B = atoi(argv[3]);
+    uint32_t B = atoi(argv[3]);
     uint32_t KERNEL = atoi(argv[4]);
     const uint8_t CHUNK = 12;
 
